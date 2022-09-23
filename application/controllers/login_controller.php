adf136cc230576e4a4b1cad3510dffdf20194949
Rifky Yusuf Mahfuz <rifkyyusufmahfud@gmail.com>
2022-09-23 15:07:34 +0700

    dashboard_noc

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -15,8 +15,18 @@ class Login_controller extends CI_Controller
 	{
 		$this->load->view('login');
 	}
+
 	public function toNocDashboard()
 	{
-		$this->load->view('noc/dashboard_noc');
+		$this->load->view('dashboard_noc/head');
+		$this->load->view('dashboard_noc/sidebar_noc');
+		$this->load->view('dashboard_noc/navbar_noc');
+		$this->load->view('dashboard_noc/konten');
+		$this->load->view('dashboard_noc/footer');
+		
+	
+	
+	
 	}
+	
 }
