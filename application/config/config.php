3b076c96a10863d35522ed038f2b644d1966c2d6
Than1st <1911500641@student.budiluhur.ac.id>
2022-09-23 14:06:42 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bast/';
+$config['base_url'] = 'http://localhost/bast_project/';
 
 /*
 |--------------------------------------------------------------------------
