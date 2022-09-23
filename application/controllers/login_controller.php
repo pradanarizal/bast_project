ead49eb197826cc48954f539af87838dfd4a99f4
Than1st <1911500641@student.budiluhur.ac.id>
2022-09-23 14:04:08 +0700

    test dashboard noc

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -3,8 +3,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login_controller extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		// $this->load->library('session');
+		// $this->load->database();
+		// $this->load->model('Model_DataUser');
+	}
 	public function index()
 	{
 		$this->load->view('login');
+	}
+	public function toNocDashboard()
+	{
+		$this->load->view('noc/dashboard_noc');
 	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -13,9 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
@@ -43,13 +41,10 @@
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
@@ -62,7 +57,7 @@
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <a href="<?php echo base_url('login_controller/toNocDashboard'); ?>" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
