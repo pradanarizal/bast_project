36a2fcadba634a7af01ee8ad4518915a1d16ac0b
Than1st <1911500641@student.budiluhur.ac.id>
2022-09-23 11:17:36 +0700

    Add Comment on Welcome,php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -22,4 +23,5 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	// add comment
 }
