c941c8a5452343e359c461ac19c43cad6ec1e8bd
Than1st <1911500641@student.budiluhur.ac.id>
2022-09-28 13:20:17 +0700

    Update db_bast.sql

--- database/db_bast.sql
+++ database/db_bast.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2022 at 07:43 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+-- Waktu pembuatan: 28 Sep 2022 pada 08.18
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,32 +25,73 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengajuan`
+-- Struktur dari tabel `executor`
 --
 
-CREATE TABLE `pengajuan` (
-  `id_pengajuan` int(3) NOT NULL,
-  `nip` varchar(10) NOT NULL,
-  `no_tiket` varchar(6) NOT NULL,
-  `nama_barang` varchar(100) NOT NULL,
-  `qty` int(2) NOT NULL,
-  `perihal` varchar(100) NOT NULL
+CREATE TABLE `executor` (
+  `id_executor` int(3) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `rekomendasi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `pengajuan`
---
-
-INSERT INTO `pengajuan` (`id_pengajuan`, `nip`, `no_tiket`, `nama_barang`, `qty`, `perihal`) VALUES
-(1, '0000000001', '124124', 'Laptop', 1, 'Gak bisa nyala'),
-(2, '0000000001', '235235', 'Komputer', 1, 'Lambat'),
-(3, '0000000001', '426235', 'Gate', 3, 'Gak bisa di pakai'),
-(4, '0000000003', '121212', 'Mouse', 5, 'Rusak');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `hardware`
+--
+
+CREATE TABLE `hardware` (
+  `id_hardware` int(3) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `id_executor` int(3) NOT NULL,
+  `komponen` varchar(50) NOT NULL,
+  `status` varchar(3) NOT NULL,
+  `problem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `requestor`
+--
+
+CREATE TABLE `requestor` (
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `bagian` varchar(50) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_tiket` int(10) NOT NULL,
+  `no_aset` varchar(20) NOT NULL,
+  `tipe_pengajuan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `requestor`
+--
+
+INSERT INTO `requestor` (`nik`, `nama`, `bagian`, `jabatan`, `keluhan`, `no_tiket`, `no_aset`, `tipe_pengajuan`) VALUES
+('3333234802010002', 'Asep', 'Rangkasbitung', 'Staff Stasiun', 'Gate Rusak', 457623, '88899988', 'hardware');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `software`
+--
+
+CREATE TABLE `software` (
+  `id_software` int(11) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `id_executor` int(3) NOT NULL,
+  `nama_software` text NOT NULL,
+  `version` varchar(20) NOT NULL,
+  `notes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -60,7 +102,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`nip`, `nama`, `role`, `password`) VALUES
@@ -73,16 +115,56 @@ INSERT INTO `user` (`nip`, `nama`, `role`, `password`) VALUES
 --
 
 --
--- Indexes for table `pengajuan`
+-- Indeks untuk tabel `executor`
 --
-ALTER TABLE `pengajuan`
-  ADD PRIMARY KEY (`id_pengajuan`);
+ALTER TABLE `executor`
+  ADD PRIMARY KEY (`id_executor`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `hardware`
+--
+ALTER TABLE `hardware`
+  ADD PRIMARY KEY (`id_hardware`),
+  ADD KEY `nik` (`nik`),
+  ADD KEY `id_executor` (`id_executor`);
+
+--
+-- Indeks untuk tabel `requestor`
+--
+ALTER TABLE `requestor`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indeks untuk tabel `software`
+--
+ALTER TABLE `software`
+  ADD PRIMARY KEY (`id_software`),
+  ADD UNIQUE KEY `id_executor` (`id_executor`),
+  ADD KEY `nik` (`nik`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`nip`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `hardware`
+--
+ALTER TABLE `hardware`
+  ADD CONSTRAINT `hardware_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `requestor` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `hardware_ibfk_2` FOREIGN KEY (`id_executor`) REFERENCES `executor` (`id_executor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `software`
+--
+ALTER TABLE `software`
+  ADD CONSTRAINT `software_ibfk_1` FOREIGN KEY (`id_executor`) REFERENCES `executor` (`id_executor`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `software_ibfk_2` FOREIGN KEY (`nik`) REFERENCES `requestor` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
