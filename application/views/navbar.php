43fccc892fe061d7cca40bf3b6e9a7b3f7219be4
pradanarizal <rizalazizpradana1@gmail.com>
2022-09-29 14:45:05 +0700

    update dashboard admin

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -11,7 +11,7 @@
             <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                 <i class="fa fa-bars"></i>
             </button>
-
+            <!-- Topbar Search -->
 
             <!-- Topbar Navbar -->
             <ul class="navbar-nav ml-auto">
