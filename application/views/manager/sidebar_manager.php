77f5b4873ee9236a8f46d6ec36c1a49fa9665239
Than1st <1911500641@student.budiluhur.ac.id>
2022-09-29 15:00:08 +0700

    current change

--- application/views/manager/sidebar_manager.php
+++ application/views/manager/sidebar_manager.php
@@ -28,36 +28,20 @@
                     <span>Dashboard</span></a>
             </li>
 
-<<<<<<< Updated upstream
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     <!-- <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"> -->
-                        <i class="fas fa-fw fa-folder"></i>
-                        <span>Pengajuan</span>
-                    </a>
-                    <!-- <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Pengajuan</span>
+                </a>
+                <!-- <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="py-2 collapse-inner rounded">
                         <a class="collapse-item" href="#">Software</a>
                         <a class="collapse-item" href="#">Hardware</a>
                     </div>
                 </div> -->
             </li>
-=======
-            <!-- Nav Item - Pages Collapse Menu
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                        <i class="fas fa-fw fa-folder"></i>
-                        <span>Pengajuan</span>
-                    </a>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                        <div class="py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Software</a>
-                            <a class="collapse-item" href="#">Hardware</a>
-                        </div>
-                    </div>
-                </li> -->
->>>>>>> Stashed changes
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
