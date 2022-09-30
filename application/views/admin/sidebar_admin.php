32d6221f550c5fe2c83558f74f9702ba67f97cb0
dolyded <dolyprahoro@gmail.com>
2022-09-30 09:59:26 +0700

    update pengajuan

--- application/views/admin/sidebar_admin.php
+++ application/views/admin/sidebar_admin.php
@@ -21,7 +21,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="<?php echo base_url('admin') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -33,10 +33,9 @@
                     <span>Pengajuan</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                    <div class="bg-gradient-primary collapse-inner rounded">
+                        <a class="collapse-item" href="<?php echo base_url('pengajuan_software') ?>" >Instalasi Software</a>
+                        <a class="collapse-item" href="<?php echo base_url('tampilan_admin/pengajuan_software') ?>" >Pengecekan Laptop/PC</a>
                     </div>
                 </div>
             </li>
@@ -48,13 +47,7 @@
                     <span>Riwayat</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
+                    
                 </div>
             </li>
             <!-- Nav Item - Pages Collapse Menu -->
