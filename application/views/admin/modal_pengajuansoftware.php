c8288ca4340062fe1ef108a08d7ab276a3a76cfc
Than1st <1911500641@student.budiluhur.ac.id>
2022-10-07 15:03:48 +0700

    fix form software

--- application/views/admin/modal_pengajuansoftware.php
+++ application/views/admin/modal_pengajuansoftware.php
@@ -11,89 +11,87 @@
                 </div>
 
                 <div class="modal-body">
-                    <form action="<?php echo site_url() . 'admin/software_save'; ?>" method="post" enctype="multipart/form-data">
-                        <h5 class="text-dark mt-3 text-center font-weight-bold">Formulir Permintaan Instalasi Software</h5>
+                    <h5 class="text-dark mt-3 text-center font-weight-bold">Formulir Permintaan Instalasi Software</h5>
+                    <div class="card">
+                        <div class="form-group mt-3">
+                            <label for="no_tiket">No Ticket</label>
+                            <input type="text" name="no_tiket" class="form-control" id="inputEmail4" placeholder="Input No. Ticket" minlength="10" value="#" required>
+                        </div>
+                    </div>
+
+                    <div class="request">
+                        <h5 class="text-dark mt-3 text-center font-weight-bold">Requestor</h5>
                         <div class="card">
-                            <div class="form-group mt-3">
-                                <label for="no_tiket">No Ticket</label>
-                                <input type="text" name="no_tiket" class="form-control" id="inputEmail4" placeholder="Input No. Ticket" minlength="10" value="#" required>
+                            <div class="form-grup mt-3">
+                                <label for="inputnik">NIK/NIP</label>
+                                <input type="text" id="inputnik" name="inputnik" class="form-control" minlength="16" required>
                             </div>
-                        </div>
 
-                        <div class="request">
-                            <h5 class="text-dark mt-3 text-center font-weight-bold">Requestor</h5>
-                            <div class="card">
-                                <div class="form-grup mt-3">
-                                    <label for="inputnik">NIK/NIP</label>
-                                    <input type="text" id="inputnik" name="inputnik" class="form-control" minlength="16" required>
-                                </div>
+                            <div class="form-grup mt-3">
+                                <label for="inputnama">Name</label>
+                                <input type="text" id="inputnama" name="inputnama" class="form-control" required>
+                            </div>
+                            <div class="form-grup mt-3">
+                                <label for="">Unit / Division</label>
+                                <input type="text" id="inputdivisi" name="inputdivisi" class="form-control" required>
+                            </div>
+                            <div class="form-grup mt-3">
+                                <label for="position">Position</label>
+                                <input type="text" id="position" name="position" class="form-control" required>
+                            </div>
 
-                                <div class="form-grup mt-3">
-                                    <label for="inputnama">Name</label>
-                                    <input type="text" id="inputnama" name="inputnama" class="form-control" required>
+                            <div class="form-grup mt-3">
+                                <label for="">Category</label><br>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="1">
+                                    <label class="form-check-label" for="inlineCheckbox1">Operating System</label>
                                 </div>
-                                <div class="form-grup mt-3">
-                                    <label for="">Unit / Division</label>
-                                    <input type="text" id="inputdivisi" name="inputdivisi" class="form-control" required>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="2">
+                                    <label class="form-check-label" for="inlineCheckbox2">Microsoft Office</label>
                                 </div>
-                                <div class="form-grup mt-3">
-                                    <label for="position">Position</label>
-                                    <input type="text" id="position" name="position" class="form-control" required>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="3">
+                                    <label class="form-check-label" for="inlineCheckbox3">Software Design</label>
                                 </div>
-
-                                <div class="form-grup mt-3">
-                                    <label for="">Category</label><br>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="1">
-                                        <label class="form-check-label" for="inlineCheckbox1">Operating System</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="2">
-                                        <label class="form-check-label" for="inlineCheckbox2">Microsoft Office</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="3">
-                                        <label class="form-check-label" for="inlineCheckbox3">Software Design</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="4">
-                                        <label class="form-check-label" for="inlineCheckbox3">Lainnya</label>
-                                    </div>
-
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="4">
+                                    <label class="form-check-label" for="inlineCheckbox3">Lainnya</label>
                                 </div>
 
-                                <div class="form-grup mt-3">
-                                    <label for="">No Asset / Inventaris / Serial Number</label>
-                                    <input type="text" name="no_asset" class="form-control" required>
-                                </div>
-                                <div class="form-grup mt-3">
-                                    <label for="">Description of Needs</label>
-                                    <textarea name="description" class="form-control" cols="30" rows="10"></textarea>
-                                </div>
-                                <div class="form-grup  mt-3">
-                                    <label for="">Signature</label>
-                                    <!-- <input type="file" name="gambar" class="form-control"> -->
-                                    <form method="post" action="process.php" enctype="multipart/form-data">
-                                        <div id="signature-pad">
-                                            <div style="border:solid 1px teal; width:360px;height:110px;padding:3px;position:relative;">
-                                                <div id="note" onmouseover="my_function();">The signature should be inside box</div>
-                                                <canvas id="the_canvas" width="350px" height="100px"></canvas>
-                                            </div>
-                                            <div style="margin:10px;">
-                                                <input type="hidden" id="signature" name="signature">
-                                                <button type="button" id="clear_btn" class="btn btn-danger" data-action="clear">Clear</button>
-                                                <button type="submit" id="save_btn" class="btn btn-success" data-action="save-png">Save</button>
-                                            </div>
+                            </div>
+
+                            <div class="form-grup mt-3">
+                                <label for="">No Asset / Inventaris / Serial Number</label>
+                                <input type="text" name="no_asset" class="form-control" required>
+                            </div>
+                            <div class="form-grup mt-3">
+                                <label for="">Description of Needs</label>
+                                <textarea name="description" class="form-control" cols="30" rows="10"></textarea>
+                            </div>
+                            <div class="form-grup  mt-3">
+                                <label for="">Signature</label>
+                                <!-- <input type="file" name="gambar" class="form-control"> -->
+                                <form method="post" action="process.php" enctype="multipart/form-data">
+                                    <div id="signature-pad">
+                                        <div style="border:solid 1px teal; width:360px;height:110px;padding:3px;position:relative;">
+                                            <div id="note" onmouseover="my_function();">The signature should be inside box</div>
+                                            <canvas id="the_canvas" width="350px" height="100px"></canvas>
                                         </div>
-                                        <form>
-                                </div>
+                                        <div style="margin:10px;">
+                                            <input type="hidden" id="signature" name="signature">
+                                            <button type="button" id="clear_btn" class="btn btn-danger" data-action="clear">Clear</button>
+                                            <button type="submit" id="save_btn" class="btn btn-success" data-action="save-png">Save</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="modal-footer  mt-3">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            <button type="SUBMIT" class="btn btn-primary">Save changes</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer  mt-3">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="SUBMIT" class="btn btn-primary">Save changes</button>
+                    </div>
                 </div>
             </div>
         </div>
